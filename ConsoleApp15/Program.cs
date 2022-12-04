e4e611e1c95734a49bb01939a611c64201c8089a
SkillFactoryCSharpTest <astafoncorpse@gmail.com>
2022-12-04 22:04:20 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp15/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] array = { { 1, 2, -3 }, { -5, 6, 7 }, { 8, 9, 10 }, { 11, 12, 13 } };
+
+            for (int i = 0; i < array.GetUpperBound(0) + 1; i++)
+            {
+                for (int k = 0; k < array.GetUpperBound(1) + 1; k++)
+                    Console.Write(array[i, k] + " ");
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
